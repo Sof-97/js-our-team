4cf0e206f95e0b00fc453baf905807355ae38f1f
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-03-24 18:47:57 +0100

    End

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-let team = [
+const team = [
     {
         'name': 'Wayne Barnett',
         'role': 'Founder & CEO',
@@ -29,4 +29,21 @@ let team = [
         'role': 'Graphic Designer',
         'image': 'barbara-ramos'
     }
-]
+];
+
+
+for (i = 0; i < team.length; i++) {
+    for ( key in team[i]) {
+        console.log(`${key}=>${team[i][key]}`)
+    }
+}
+
+for(i=0;i<team.length; i++){
+    document.getElementById('container').innerHTML +=(`<div class="card col">
+        <img src="./assets/img/${team[i]['image']}.jpg" class="card-img-top" alt="portrait"/>
+        <div class="card-body">
+            <h3 class="card-title text-center">${team[i]['name']}</h3>
+            <p class="card-text text-center fw-lighter">${team[i]['role']}</p>
+        </div>
+    </div>`)
+}
