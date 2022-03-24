bafc0d7500b36ca8d18f1e66f0febbdfdb0b599e
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-03-24 18:16:48 +0100

    done html

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+let team = [
+    {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'image': 'wayne-barnett'
+    },
+    {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'image': 'angela-caroll'
+    },
+    {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'image': 'walter-gordon'
+    },
+    {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'image': 'angela-lopez'
+    },
+    {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'image': 'scott-estrada'
+    },
+    {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'image': 'barbara-ramos'
+    }
+]
